d40c63aea38ada5051d23ed6427cea7868d14d9a
awesomelewis2007 <60709927+awesomelewis2007@users.noreply.github.com>
2021-05-09 10:37:34 +0100

    Delete installer.py

--- installer.py
+++
@@ -1,5 +0,0 @@
-import os
-input("welcome to the Xshell installer \npress crtl+C to quit without making changes or press enter to start >>")
-os.system("pip install colr")
-os.system("pip install tcping")
-os.system("pip install psutil")
